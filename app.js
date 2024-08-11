b9536d4b7afd17bbd37ee228b43dc591db9aff8b
Benjamin <benjb828@gmail.com>
2024-08-10 21:14:00 -0400

    install mysql

---
+++ app.js
@@ -0,0 +1,25 @@
+const block1 = document.getElementById('block1')
+const block2 = document.getElementById('block2')
+const block3 = document.getElementById('block3')
+const block4 = document.getElementById('block4')
+const block5 = document.getElementById('block5')
+const name = document.getElementById('name')
+const submit = document.getElementById('submit')
+
+submit.addEventListener('click', () => {
+    submitSchedule()
+})
+var mysql = require('mysql');
+
+var con = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "root"
+});
+console.log('run')
+con.connect(function(err) {
+  if (err) throw err;
+  console.log("Connected!");
+});
+
+submitSchedule()
